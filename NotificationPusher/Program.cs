1a6d2e21efce65a6a5b1c5cfe0a14acf3e27dda1
Rafael Pereira <rafael.pereira@mobrj.com.br>
2019-02-23 23:59:39 -0300

    Updated to latest API and incluced command line program to push notifications

---
+++ NotificationPusher/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace NotificationPusher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string cargo = "";
+            const string cpf = "";
+            const string deviceId = "";
+
+            //ServicoNotificacao
+            //    .EnviarNotificacaoLogoutAsync(cpf, deviceId)
+            //    .Wait();
+            //ServicoNotificacao
+            //    .EnviarNotificacaoAtualizacaoPorCargoAsync(cargo)
+            //    .Wait();
+            ServicoNotificacao
+                .EnviarNotificacaoAtualizacaoPorCpfAsync(cpf)
+                .Wait();
+        }
+    }
+}
